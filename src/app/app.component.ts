584b2f01f4b9a69f5b6b86ccc7e2b174274b6cb2
Daomon <104716601+Daomon@users.noreply.github.com>
2022-12-16 02:01:11 +0100

    4 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import 'particles.js/particles';
+const particlesJS = (<any>window).particlesJS;
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   name = 'Angular ' + VERSION.major;
+
+  ngOnInit() {
+    particlesJS.load('particles-js', '../assets/particles.json', function() {
+      console.log('callback - particles.js config loaded');
+    });
+  }
 }
